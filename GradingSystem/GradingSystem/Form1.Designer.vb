a4e7c3a070d9a6aac12ccc3b4758bf16ad921ad9
CodexNoob <115176146+CodexNoob@users.noreply.github.com>
2023-04-28 23:47:54 +0800

    Add files via upload

---
+++ GradingSystem/GradingSystem/Form1.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        TextBox3 = New TextBox()
+        Label3 = New Label()
+        TextBox4 = New TextBox()
+        Label4 = New Label()
+        TextBox5 = New TextBox()
+        Label5 = New Label()
+        TextBox6 = New TextBox()
+        Label6 = New Label()
+        TextBox7 = New TextBox()
+        Label7 = New Label()
+        TextBox8 = New TextBox()
+        Label8 = New Label()
+        TextBox9 = New TextBox()
+        Label9 = New Label()
+        TextBox10 = New TextBox()
+        Label10 = New Label()
+        TextBox11 = New TextBox()
+        Label11 = New Label()
+        TextBox12 = New TextBox()
+        Label12 = New Label()
+        TextBox13 = New TextBox()
+        Label13 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(43, 45)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(112, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter Assignment 1:"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(161, 37)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(161, 76)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(43, 84)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(109, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Enter Assignment2:"' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(161, 117)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(43, 125)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(112, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Enter Assignment 3:"' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(161, 157)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 7
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(43, 165)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(73, 15)
+        Label4.TabIndex = 6
+        Label4.Text = "Enter Quiz 1:"' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(161, 198)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 9
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(43, 206)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(73, 15)
+        Label5.TabIndex = 8
+        Label5.Text = "Enter Quiz 2:"' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(161, 241)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 11
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(43, 249)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(73, 15)
+        Label6.TabIndex = 10
+        Label6.Text = "Enter Quiz 3:"' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(161, 283)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 13
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(43, 291)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(93, 15)
+        Label7.TabIndex = 12
+        Label7.Text = "Enter Recitation:"' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(161, 325)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(100, 23)
+        TextBox8.TabIndex = 15
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(43, 333)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(89, 15)
+        Label8.TabIndex = 14
+        Label8.Text = "Enter Activity 1:"' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(161, 368)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(100, 23)
+        TextBox9.TabIndex = 17
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(43, 376)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(89, 15)
+        Label9.TabIndex = 16
+        Label9.Text = "Enter Activity 2:"' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(161, 412)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(100, 23)
+        TextBox10.TabIndex = 19
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(43, 420)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(89, 15)
+        Label10.TabIndex = 18
+        Label10.Text = "Enter Activity 3:"' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(524, 42)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(100, 23)
+        TextBox11.TabIndex = 21
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(407, 50)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(101, 15)
+        Label11.TabIndex = 20
+        Label11.Text = "Enter Attendance:"' 
+        ' TextBox12
+        ' 
+        TextBox12.Location = New Point(524, 81)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(100, 23)
+        TextBox12.TabIndex = 23
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(407, 89)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(103, 15)
+        Label12.TabIndex = 22
+        Label12.Text = "Enter Exam Grade:"' 
+        ' TextBox13
+        ' 
+        TextBox13.Location = New Point(524, 330)
+        TextBox13.Name = "TextBox13"
+        TextBox13.Size = New Size(100, 23)
+        TextBox13.TabIndex = 25
+        ' 
+        ' Label13
+        ' 
+        Label13.AccessibleName = "Total"
+        Label13.AutoSize = True
+        Label13.Location = New Point(380, 338)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(138, 15)
+        Label13.TabIndex = 24
+        Label13.Text = "Yur Total Prelim Grade is:"' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(381, 165)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(255, 36)
+        Button1.TabIndex = 26
+        Button1.Text = "CLKICK TO COMPUTE GRADE"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 610)
+        Controls.Add(Button1)
+        Controls.Add(TextBox13)
+        Controls.Add(Label13)
+        Controls.Add(TextBox12)
+        Controls.Add(Label12)
+        Controls.Add(TextBox11)
+        Controls.Add(Label11)
+        Controls.Add(TextBox10)
+        Controls.Add(Label10)
+        Controls.Add(TextBox9)
+        Controls.Add(Label9)
+        Controls.Add(TextBox8)
+        Controls.Add(Label8)
+        Controls.Add(TextBox7)
+        Controls.Add(Label7)
+        Controls.Add(TextBox6)
+        Controls.Add(Label6)
+        Controls.Add(TextBox5)
+        Controls.Add(Label5)
+        Controls.Add(TextBox4)
+        Controls.Add(Label4)
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ GradingSystem/GradingSystem/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Dim asign As Double
+    Dim quiz As Double
+    Dim recite As Double
+    Dim act As Double
+    Dim att As Double
+    Dim exam As Double
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox11_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        asign = (Val(TextBox1.Text) + Val(TextBox2.Text) + Val(TextBox3.Text) * 0.1 / 3)
+        quiz = (Val(TextBox4.Text) + Val(TextBox5.Text) + Val(TextBox6.Text) * 0.2 / 3)
+        recite = (Val(TextBox7.Text) * 0.15)
+        act = (Val(TextBox8.Text) + Val(TextBox9.Text) + Val(TextBox10.Text) * 0.1 / 3)
+        att = (Val(TextBox11.Text) * 0.05)
+        exam = (Val(TextBox12.Text) * 0.4)
+
+        TextBox13.Text = (asign + quiz + recite + act + att + exam) / 6
+    End Sub
+
+    Private Sub Label13_Click(sender As Object, e As EventArgs) Handles Label13.Click
+
+    End Sub
+
+    Private Sub TextBox13_TextChanged(sender As Object, e As EventArgs) Handles TextBox13.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
